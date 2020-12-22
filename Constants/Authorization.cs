ddeb9081cc2a75883f554690194c21a5934d5d3c
Jurgen Emanuels <Jurgen.Emanuels@CyberIAM.com>
2020-12-22 16:06:33 +0200

    I have added the seed data file and the Authorixation with the different roles

---
+++ Constants/Authorization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jurmen.Constants
+{
+    public class Authorization
+    {
+        public enum Roles
+        {
+            Administrator,
+            Moderator,
+            User
+        }
+
+
+        public const string default_username = "user";
+        public const string default_email = "user@localhost.com";
+        public const string default_password = "Pa$$w0rd.";
+        public const Roles default_role = Roles.User;
+
+
+    }
+}
